851304231d7fdf4ec167c449944a6e6d7f3b3d0f
RekenGit <72222392+RekenGit@users.noreply.github.com>
2023-04-03 22:50:08 +0200

    Create project on git
    Add all files to git rep

---
+++ Crypto/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Crypto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(420, 461);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(564, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(420, 461);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Encrypt from";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.ForeColor = System.Drawing.Color.LightGreen;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "2",
+            "8",
+            "8 - Crypto (old)",
+            "8 - Crypto",
+            "10",
+            "16",
+            "32 - GeoHash",
+            "64",
+            "64 - KeyBoard"});
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 20);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(144, 150);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.LightGreen;
+            this.button1.Location = new System.Drawing.Point(0, 412);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.checkedListBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox2.CheckOnClick = true;
+            this.checkedListBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkedListBox2.ForeColor = System.Drawing.Color.LightGreen;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "2",
+            "8",
+            "8 - Crypto (old)",
+            "8 - Crypto",
+            "10",
+            "16",
+            "32 - GeoHash",
+            "64",
+            "64 - KeyBoard"});
+            this.checkedListBox2.Location = new System.Drawing.Point(0, 20);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(144, 150);
+            this.checkedListBox2.TabIndex = 6;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Decrypt to";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(420, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(144, 461);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.checkedListBox1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 170);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(144, 170);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkedListBox2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(144, 170);
+            this.panel2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1000, 500);
+            this.Name = "Form1";
+            this.Text = "Crypto";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Crypto/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crypto
+{
+    public partial class Form1 : Form
+    {
+        int Zaznaczony1 = 0, Zaznaczony2 = 0, InpLeng = 0;
+        string Binary, Inp = "";
+        bool stop, upper;
+        Control conBox1, conBox2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            checkedListBox1.SetItemChecked(0, true);
+            checkedListBox2.SetItemChecked(0, true);
+            conBox1 = textBox1;
+            conBox2 = textBox2;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+            double x = (control.Size.Width - 156) / 2;
+            int size = Convert.ToInt32(Math.Round(x));  
+            conBox1.Size = new Size(size, control.Size.Height);
+            conBox2.Size = new Size(size, control.Size.Height);
+        }
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Zaznaczony1 = checkedListBox1.SelectedIndex;
+            int Selected = checkedListBox1.Items.Count;
+            for (int x = 0; x < Selected; x++) if (Zaznaczony1 != x) checkedListBox1.SetItemChecked(x, false);
+        }
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Zaznaczony2 = checkedListBox2.SelectedIndex;
+            int Selected = checkedListBox2.Items.Count;
+            for (int x = 0; x < Selected; x++) if (Zaznaczony2 != x) checkedListBox2.SetItemChecked(x, false);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Inp = textBox1.Text;
+            textBox2.Text = Binary = "";
+            stop = false;
+            upper = true;
+            switch (Zaznaczony1) 
+            {
+                case 0:
+                    BinToDec();
+                    break;
+                case 1:
+                    NaBinarny(Oct, B8, "0 1 2 3 4 5 6 7", 8);
+                    break;
+                case 2:
+                    NaBinarny(Cry, B8, "` ,  . | ' \" ! -", 8);
+                    break;
+                case 3:
+                    NaBinarny(CryNew, B8, ". :  , ; ! | ' `", 8);
+                    break;
+                case 4:
+                    DecToBin();
+                    break;
+                case 5:
+                    NaBinarny(Hex, B16, "0 1 2 3 4 5 6 7 8 9 A B C D E F", 16);
+                    break;
+                case 6:
+                    NaBinarny(Geo, B32, "0 1 2 3 4 5 6 7 8 9 B C D E F G H J K M N P Q R S T U V W X Y Z", 32);
+                    break;
+                case 7:
+                    upper = false;
+                    NaBinarny(Tet, B64, "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 + /", 64);
+                    break;
+                case 8:
+                    NaBinarny(Key, B64, "0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z . , _ ! ? % [ { ( < > ) } ] @ # * / \\ + - = ^ : ; ' \" oraz spcaja", 64);
+                    break;
+            }
+            if (!stop)
+            {
+                switch (Zaznaczony2)
+                {
+                    case 0:
+                        textBox2.Text = Binary;
+                        break;
+                    case 1:
+                        ZBinarnego(Oct, B8, 8, 3);
+                        break;
+                    case 2:
+                        ZBinarnego(Cry, B8, 8, 3);
+                        break;
+                    case 3:
+                        ZBinarnego(CryNew, B8, 8, 3);
+                        break;
+                    case 4:
+                        BinToDec();
+                        break;
+                    case 5:
+                        ZBinarnego(Hex, B16, 16, 4);
+                        break;
+                    case 6:
+                        ZBinarnego(Geo, B32, 32, 5);
+                        break;
+                    case 7:
+                        ZBinarnego(Tet, B64, 64, 6);
+                        break;
+                    case 8:
+                        ZBinarnego(Key, B64, 64, 6);
+                        break;
+                }
+            }
+        }
+        /* 
+            (2) Binarny           - Bin
+            (8) Octal             - Oct
+            (8) Crypto Old        - Cry
+            (8) Crypto            - CryNew
+            (10) Decimal          - Dec
+            (16) Hexadecimal      - Hex
+            (32) Geohash          - Geo
+            (64) Tetrasexagesimal - Tet
+            (64) KeyBoard         - Key
+        */
+        //Biblioteka znaków
+        string[] B8 = {"000", "001", "010", "011", "100", "101", "110", "111"
+        };
+        string[] B16 = {"0000", "0001", "0010", "0011", "0100", "0101", "0110",
+            "0111", "1000", "1001","1010", "1011", "1100", "1101", "1110", "1111"};
+        string[] B32 = {"00000", "00001", "00010", "00011", "00100", "00101", "00110",
+            "00111", "01000", "01001", "01010", "01011", "01100", "01101", "01110", "01111",
+            "10000", "10001", "10010", "10011", "10100", "10101", "10110", "10111", "11000",
+            "11001", "11010", "11011", "11100", "11101", "11110", "11111"};
+        string[] B64 = {"000000", "000001", "000010", "000011", "000100", "000101", "000110",
+            "000111", "001000", "001001","001010", "001011", "001100", "001101", "001110",
+            "001111", "010000", "010001", "010010", "010011", "010100", "010101", "010110",
+            "010111", "011000", "011001", "011010", "011011", "011100", "011101", "011110",
+            "011111", "100000", "100001", "100010", "100011", "100100", "100101", "100110",
+            "100111", "101000", "101001", "101010", "101011", "101100", "101101", "101110",
+            "101111", "110000", "110001", "110010", "110011", "110100", "110101", "110110",
+            "110111", "111000", "111001", "111010", "111011", "111100", "111101", "111110",
+            "111111"};
+        string[] Oct = {"0", "1", "2", "3", "4", "5", "6", "7"
+        };
+        string[] Cry = {"`", ",", ".", "|", "'", "\"", "!", "-"
+        };
+        string[] CryNew = {".", ":", ",", ";", "!", "|", "'", "`"
+        };
+        string[] Hex = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F"};
+        string[] Geo = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "B", "C", "D", "E", "F", "G", "H", "J", "K", "M", "N", "P", "Q",
+            "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        string[] Tet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z", "a", "b",
+            "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q",
+            "r", "s","t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3","4", "5",
+            "6", "7", "8", "9", "+", "/"};
+        string[] Key = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A", "B", "C",
+            "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S",
+            "T", "U", "W", "Z", "Q", "V", "X", "Y", ".", ",", "_", "!", "?", "%", "(",
+            ")", "[", "]", "{", "}", "<", ">", "@", "#", "/", "\\", "*", "+", "-", "=",
+            "^", ":", ";", "\"", "'", " "};
+        //Podstawy
+        void NaBinarny(string[] znaki, string[] bin, string error, int ileZnak)
+        {
+            InpLeng = Inp.Length;
+            for (int i = 0; i < InpLeng; i++)
+            {
+                string znak = "";
+                for (int x = 0; x < ileZnak; x++)
+                {
+                    if (upper == true) { if (Inp[i].ToString().ToUpper() == znaki[x]) znak = bin[x]; }
+                    else { if (Inp[i].ToString() == znaki[x]) znak = bin[x]; }
+                }
+                if (znak == "")
+                {
+                    textBox2.Text = "Tylko te znaki: \r\n" + error;
+                    stop = true;
+                    break;
+                }
+                Binary += znak;
+            }
+        }
+        void ZBinarnego(string[] znaki, string[] bin, int ileZnak, int zera)
+        {
+            if (Binary == "0") textBox2.Text = znaki[0];
+            else
+            {
+                int BinLeng = Binary.Length;
+                string RewBin = Binary, calosc = "";
+                while (BinLeng > 0)
+                {
+                    string znak = "";
+                    if (BinLeng < zera)
+                    {
+                        string FirstBit = "";
+                        bool usun = true;
+                        for (int i = 0; i < zera - BinLeng; i++) FirstBit = "0" + FirstBit;
+                        for (int i = BinLeng; i >= 1; i--)
+                        {
+                            if (RewBin[BinLeng - i].ToString() == "1") usun = false;
+                            znak += RewBin[BinLeng - i];
+                        }
+                        if (!usun)
+                        {
+                            znak = FirstBit + znak;
+                            for (int x = 0; x < ileZnak; x++) if (znak == bin[x]) znak = znaki[x];
+                            calosc = znak + calosc;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = zera; i >= 1; i--) znak += RewBin[BinLeng - i];
+                        for (int x = 0; x < ileZnak; x++) if (znak == bin[x]) znak = znaki[x];
+                        calosc = znak + calosc;
+                    }
+                    BinLeng -= zera;
+                }
+                textBox2.Text += calosc;
+            }
+        }
+        static IEnumerable<string> Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
+        }
+        void DecToBin() 
+        {
+            int maxNumber = 8, tempNumber = 0;
+            int divideCount = Inp.Length % maxNumber == 0 ? Inp.Length / maxNumber : Convert.ToInt32(Math.Round(Inp.Length / maxNumber * 1.0) + 1);
+            long[] numbers = new long[divideCount];
+            for (int i = 0; i < divideCount; i++)
+            {
+                if (i == divideCount - 1) numbers[i] = Convert.ToInt64(Inp.Substring(tempNumber, Inp.Length - maxNumber * (divideCount - 1)));
+                else numbers[i] = Convert.ToInt64(Inp.Substring(tempNumber, maxNumber));
+                tempNumber += maxNumber;
+            }
+            while (numbers[divideCount - 1] < 1)
+            {
+                foreach (long item in numbers)
+                {
+
+                }
+            }
+
+            /*if (Inp.Length <= 10)
+            {
+                try
+                {
+                    long liczba = 0;
+                    if (Inp == "0") Binary = "0";
+                    else liczba = long.Parse(Inp);
+                    while (liczba > 0)
+                    {
+                        if (liczba % 2 == 0) Binary = 0 + Binary;
+                        else
+                        {
+                            liczba--;
+                            Binary = 1 + Binary;
+                        }
+                        liczba /= 2;
+                    }
+                }
+                catch
+                {
+                    textBox2.Text = "Tylko te znaki: \r\n0 1 2 3 4 5 6 7 8 9"; 
+                    stop = true;
+                }
+            } 
+            else
+            {
+                textBox2.Text = "Liczba może mieć maksymalnie 10 znaków, takie są ograniczenia w systemie dziesiętnym";
+                stop = true;
+            }*/
+        }
+        void BinToDec()
+        {
+            long wynik = 0;
+            for (int i = 0; i < Binary.Length; i++)
+            {
+                if (Binary[i] == '0') wynik *= 2;
+                else if (Binary[i] == '1') wynik = wynik * 2 + 1;
+                else
+                {
+                    textBox2.Text += "Tylko znaki: \r\n0 1";
+                    stop = true;
+                }
+            }
+            textBox2.Text = wynik.ToString();
+        }
+    }
+}
